b5ceaf55a7ee782be2f0c179cb69c961a1870241
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-01-15 00:21:15 +0900

    setting: 프로젝트 초기 설정

---
+++ src/main/java/umc/kittenback/KittenBackApplication.java
@@ -0,0 +1,13 @@
+package umc.kittenback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittenBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittenBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/kittenback/KittenBackApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.kittenback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittenBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
