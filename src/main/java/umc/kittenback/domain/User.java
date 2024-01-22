e76ea7d4427104dd5c949e1341e5bd9aa61df165
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-01-23 07:34:08 +0900

    feat(User): User entity 생성

---
+++ src/main/java/umc/kittenback/domain/User.java
@@ -0,0 +1,57 @@
+package umc.kittenback.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import umc.kittenback.domain.enums.OAuth2Provider;
+import umc.kittenback.domain.enums.UserRole;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+public class User extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 이메일
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // 이름
+    @Column(nullable = false)
+    private String name;
+
+    // 휴대폰 번호
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    // OAuth2 제공자
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OAuth2Provider provider;
+
+    // OAuth2 제공자의 사용자 고유 ID
+    private String providerId;
+
+    // 사용자 이미지
+    private String profileImage;
+}
---
+++ src/main/java/umc/kittenback/domain/enums/UserRole.java
@@ -0,0 +1,5 @@
+package umc.kittenback.domain.enums;
+
+public enum UserRole {
+    ROLE_ADMIN, ROLE_USER
+}
