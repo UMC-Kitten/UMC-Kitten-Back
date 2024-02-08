df3efc651a84dbd1cba1f1e038804dc1e615bfbe
ByeongGyu Jeon <gentle9828@naver.com>
2024-02-08 20:29:51 +0900

    Revert "feat: 애플 로그인 구현"

--- src/main/java/umc/kittenback/response/code/status/ErrorStatus.java
+++ src/main/java/umc/kittenback/response/code/status/ErrorStatus.java
@@ -19,11 +19,7 @@ public enum ErrorStatus implements BaseErrorCode {
     // User 관련 에러
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER4001", "사용자가 없습니다."),
     NICKNAME_INPUT_NOT_EXIST(HttpStatus.BAD_REQUEST, "USER4002", "닉네임을 입력해주세요"),
-    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "USER4003", "이미 사용중인 닉네임입니다."),
-
-    FAIL_PARSE_APPLE_IDENTITY_TOKEN(HttpStatus.BAD_REQUEST, "TOKEN4001", "Apple Identity Token 헤더 파싱을 실패하였습니다."),
-    FAIL_PARSE_CLAIM(HttpStatus.BAD_REQUEST, "TOKEN4002", "Claim을 파싱하는데 실패하였습니다."),
-    INVALID_APPLE_OAUTH_CLAIMS(HttpStatus.BAD_REQUEST, "TOKEN4003", "유효하지 않은 Apple OAuth Claim입니다.");
+    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "USER4003", "이미 사용중인 닉네임입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
