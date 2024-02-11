b2776361d74c10b2d0fb1d2a2c472633d3612143
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-02-11 19:42:58 +0900

    feat(healthNote) : HealthNote/Image 도메인 엔티티

--- src/main/java/umc/kittenback/domain/HealthNote.java
+++ src/main/java/umc/kittenback/domain/HealthNote.java
@@ -1,5 +1,9 @@
 package umc.kittenback.domain;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -9,16 +13,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.type.BigIntegerType;
+import lombok.Setter;
 import umc.kittenback.domain.enums.HealthNoteType;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -39,16 +45,19 @@ public class HealthNote {
     // 건강수첩 제목
     private String title;
 
+    // 건강수첩 기록 시간
+    private LocalDate date;
+
     //건강수첩 병원
     private String hospital;
 
-    //건강수첩 날짜
-
     //건강수첩 비용
-    private BigIntegerType cost;
+    private Integer cost;
 
     //건강수첩 내용
     private String content;
 
-
+    //건강수첩 이미지들
+    @OneToMany(mappedBy = "healthNote", cascade = CascadeType.ALL)
+    private List<HealthNoteImage> imageList = new ArrayList<>();
 }
---
+++ src/main/java/umc/kittenback/domain/HealthNoteImage.java
@@ -0,0 +1,38 @@
+package umc.kittenback.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class HealthNoteImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "healthNote_id")
+    private HealthNote healthNote;
+
+    public HealthNoteImage(String imageUrl) {
+        this.image = imageUrl;
+    }
+}
