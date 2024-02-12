ba128f4019142753560439e83a466b7dc351055d
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-02-13 05:16:51 +0900

    refactor(KakaoUserService): 연동 테스트를 위한 로직 수정

--- src/main/java/umc/kittenback/service/pet/PetServiceImpl.java
+++ src/main/java/umc/kittenback/service/pet/PetServiceImpl.java
@@ -3,7 +3,6 @@ package umc.kittenback.service.pet;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.ErrorState;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.kittenback.domain.Pet;
