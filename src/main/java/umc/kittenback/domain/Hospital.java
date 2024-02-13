7be0e0f8cb37c32dd559500de516f292d900ca34
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-02-13 20:57:07 +0900

    feat(Hosptial): name, address 검색 repository

--- src/main/java/umc/kittenback/domain/Hospital.java
+++ src/main/java/umc/kittenback/domain/Hospital.java
@@ -1,2 +1,33 @@
-package umc.kittenback.domain;public class Hospital {
+package umc.kittenback.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import umc.kittenback.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Hospital extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
 }
--- src/main/java/umc/kittenback/repository/HospitalRepository.java
+++ src/main/java/umc/kittenback/repository/HospitalRepository.java
@@ -1,2 +1,9 @@
-package umc.kittenback.repository;public interface HospitalRepository {
+package umc.kittenback.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.kittenback.domain.Hospital;
+
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+    List<Hospital> findByNameContainingOrAddressContaining(String name, String address);
 }
