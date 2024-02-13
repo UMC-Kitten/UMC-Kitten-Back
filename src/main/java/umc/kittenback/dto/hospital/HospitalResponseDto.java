0d83741f8bb731896c8f15bf8ef01ce57b9c17cc
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-02-13 20:58:52 +0900

    feat(Hosptial): HosptialResponse Dto 생성

--- src/main/java/umc/kittenback/dto/hospital/HospitalResponseDto.java
+++ src/main/java/umc/kittenback/dto/hospital/HospitalResponseDto.java
@@ -1,9 +1,13 @@
 package umc.kittenback.dto.hospital;
 
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
+import umc.kittenback.domain.Hospital;
 
 @Getter
 @Builder
-public class HospitalSearchDto {
+public class HospitalResponseDto {
+    private final Integer resultNum; // 검색된 병원 수
+    private List<Hospital> hospitals; // 병원 정보들
 }
